54e214eb21f45871835e7c452103b14f8f0aa69f
Thiago <tsuna2221@live.com>
2019-05-03 01:09:07 -0300

    Sidebar Modulartization

--- src/components/Market/PartComponents/CategoryPath/Sidebar.js
+++ src/components/Market/PartComponents/CategoryPath/Sidebar.js
@@ -1,114 +1,20 @@
 import React, { Component } from 'react';
 
-import { getAnchor } from '../../Partials/queryPartials'
+import SideSubs from './SidebarComps/SideSubs'
+import SideManufact from './SidebarComps/SideManufact'
 
 class Sidebar extends Component {
     render() {
         return (
             <div className='cat-sidebar pad-t-20'>
-                {this.drawCategory()}
-                {this.drawManufacturer()}
+                <SideSubs category={this.props.category.data} param={this.props.match.params.subcategory} typeList={this.props.products.list_types}/>
+                <SideManufact category={this.props.category.data} companies={this.props.products.list_companies}/>
             </div>
         );
     }
 
     state = {
-        manufactList: {
-            active: false,
-            limit: 6
-        },
-        typesList: {
-            active: false,
-            limit: 6
-        }
-    }
-
-    drawCategory = () => {
-        if(this.props.category.data){
-            var { category_name, sub_categories } = this.props.category.data
-            var subParam = this.props.match.params.subcategory
-            var listState = this.state.typesList
-            var typos = []
-    
-            var subs = sub_categories.map((sub, index) => (
-                <li key={category_name.replace(/\s/g, '-') + index} className="mar-v-4">
-                    <a href={`/c/${category_name.replace(/\s/g, '-') + "/" + sub.name.replace(/\s/g, '-')}`} className="s-13 w-medium c-light3 clickable t-colorize-text">{sub.name}</a>
-                </li>
-            ))
-
-            this.props.products.list_types.forEach(typo => {
-                if(!typos.includes(typo)){
-                    typos.push(typo)
-                }
-            })
-            
-            var types = typos.map((type, index) => index < listState.limit ? 
-                <li key={category_name.replace(/\s/g, '-') + index} className="mar-v-4">
-                    <a href={getAnchor("type", type)} className="s-13 w-medium c-light3 clickable t-colorize-text">{type}</a>
-                </li> : null
-            )
-
-            if(subParam){
-                return (
-                    <div className="s-listing">
-                        <label className="s-14 w-medium c-blue">Types</label>
-                        <ul className="mar-l-20">
-                            {types}
-                        </ul>
-                    </div>
-                )
-            }else{
-                return (
-                    <div className="s-listing">
-                        <label className="s-14 w-medium c-blue">{category_name}</label>
-                        <ul className="mar-l-20">
-                            {subs}
-                        </ul>
-                    </div>
-                )
-            }
-        }
-    }
-
-    drawManufacturer = () => {
-        if(this.props.category.data){
-            var { category_name } = this.props.category.data
-            var listState = this.state.manufactList
-            var manufacturers = []
-            var buttonLabel = listState.active ? "Show Less" : 'Show More'
-            var showButton = () => {
-                if(manufacturers.length > 0){
-                    return manufacturers.length < 6 ? null : (<span onClick={lengthActivation} className="s-13 w-medium c-blue clickable">{buttonLabel}</span>)
-                }
-            }
-            var lengthActivation = () => listState.active ? this.setState({manufactList:{limit: 6, active: false}}) : this.setState({manufactList:{limit: manufacturers.length, active: true}})
-    
-            this.props.products.list_companies.forEach(manufacturer => {
-                if(!manufacturers.includes(manufacturer)){
-                    manufacturers.push(manufacturer)
-                }
-            })
-    
-            var manufact = manufacturers.map((manu, index) => {
-                if(index < listState.limit){
-                    return (
-                        <li key={category_name.replace(/\s/g, '-') + index} className="mar-v-4">
-                            <a href={getAnchor("manufacturer", manu)} className="s-13 w-medium c-light3 clickable t-colorize-text">{manu}</a>
-                        </li>
-                    )
-                }
-            })
-    
-            return (
-                <div className="s-listing">
-                    <label className="s-14 w-medium c-blue">Manufacturer</label>
-                    <ul className="mar-l-20">
-                        {manufact}
-                        {showButton()}
-                    </ul>
-                </div>
-            )
-        }
+        
     }
 
     drawRating = () => {
---
+++ src/components/Market/PartComponents/CategoryPath/SidebarComps/SideManufact.js
@@ -0,0 +1,63 @@
+import React, { Component, Fragment } from 'react';
+import { getAnchor } from "../../../Partials/queryPartials"
+
+class SideManufact extends Component {
+    render() {
+        return (
+            <Fragment>
+                {this.drawManufacturer()}
+            </Fragment>
+        );
+    }
+
+    state = {
+        manufactList: {
+            active: false,
+            limit: 6
+        }
+    }
+
+    drawManufacturer = () => {
+        if(this.props.category){
+            var { category_name } = this.props.category
+            var manufacturers = []
+            var listState = this.state.manufactList
+            var buttonLabel = listState.active ? "Show Less" : 'Show More'
+            var showButton = () => {
+                if(manufacturers.length > 0){
+                    return manufacturers.length < 6 ? null : (<span onClick={lengthActivation} className="s-13 w-medium c-blue clickable">{buttonLabel}</span>)
+                }
+            }
+
+            var lengthActivation = () => listState.active ? this.setState({manufactList:{limit: 6, active: false}}) : this.setState({manufactList:{limit: manufacturers.length, active: true}})
+
+            this.props.companies.forEach(manufacturer => {
+                if(!manufacturers.includes(manufacturer)){
+                    manufacturers.push(manufacturer)
+                }
+            })
+    
+            var manufact = manufacturers.map((manu, index) => {
+                if(index < listState.limit){
+                    return (
+                        <li key={category_name.replace(/\s/g, '-') + index} className="mar-v-4">
+                            <a href={getAnchor("manufacturer", manu)} className="s-13 w-medium c-light3 clickable t-colorize-text">{manu}</a>
+                        </li>
+                    )
+                }
+            })
+    
+            return (
+                <div className="s-listing">
+                    <label className="s-14 w-medium c-blue">Manufacturer</label>
+                    <ul className="mar-l-20">
+                        {manufact}
+                        {showButton()}
+                    </ul>
+                </div>
+            )
+        }
+    }
+}
+
+export default SideManufact;
