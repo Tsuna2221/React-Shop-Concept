df8119dae049c2da616588068ad82d3759ec3c7a
Thiago <tsuna2221@live.com>
2019-05-06 06:57:54 -0300

    Updates

---
+++ precache-manifest.ae85e12af7a683dc8d61dc3957c553d4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b9237dc307fecc8217f",
+    "url": "/react-shop-concept/static/css/main.eac239d8.chunk.css"
+  },
+  {
+    "revision": "4b9237dc307fecc8217f",
+    "url": "/react-shop-concept/static/js/main.cdccf283.chunk.js"
+  },
+  {
+    "revision": "af52501a217f4d058aab",
+    "url": "/react-shop-concept/static/js/runtime~main.363fc00f.js"
+  },
+  {
+    "revision": "c94cc35fc99331ee2139",
+    "url": "/react-shop-concept/static/js/2.f7c83552.chunk.js"
+  },
+  {
+    "revision": "b50deb1c1eb888a9089c2dc096938b3d",
+    "url": "/react-shop-concept/static/media/star.b50deb1c.svg"
+  },
+  {
+    "revision": "b0f1ee7be31fea7fee12865235ac64e1",
+    "url": "/react-shop-concept/static/media/star-outline.b0f1ee7b.svg"
+  },
+  {
+    "revision": "b2fcbcd8a957ea5166a1ee39d098e37d",
+    "url": "/react-shop-concept/static/media/computers.b2fcbcd8.png"
+  },
+  {
+    "revision": "fe1723c909a0fcdbe579a62d24a76130",
+    "url": "/react-shop-concept/static/media/games.fe1723c9.png"
+  },
+  {
+    "revision": "623403f2015adf14e8552856dfe9f529",
+    "url": "/react-shop-concept/static/media/phones.623403f2.png"
+  },
+  {
+    "revision": "2539ce811c455f3620835c1498a3580c",
+    "url": "/react-shop-concept/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/react-shop-concept/precache-manifest.ae85e12af7a683dc8d61dc3957c553d4.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/react-shop-concept/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
